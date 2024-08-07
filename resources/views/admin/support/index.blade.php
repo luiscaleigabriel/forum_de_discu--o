6c6eec07c7ffca46759980839ccd45b973b691cb
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-08-07 11:31:57 +0100

    finalizando os status

--- resources/views/admin/support/index.blade.php
+++ resources/views/admin/support/index.blade.php
@@ -15,7 +15,7 @@
     @foreach($supports->items() as $support)
     <tr>
         <td>{{ $support->subject }}</td>
-        <td>{{ $support->status }}</td>
+        <td>{{ getStatusSupport($support->status) }}</td>
         <td>{{ $support->body }}</td>
         <td>
         <a href="{{ route('support.show', $support->id) }}"> > </a> |
